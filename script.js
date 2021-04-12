d8227837d03533a4b288e1049156688690aeea38
emile-voiron <75317530+emile-voiron@users.noreply.github.com>
2021-04-12 13:06:05 +0000

    Deploying to gh-pages from master @ a0830c18b92df447d85ffd321f8cf059acd83552 🚀

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-(()=>{"use strict";console.log("Script started successfully"),WA.openCoWebSite("https://workadventu.re")})();
+(()=>{"use strict";console.log("Script started successfully"),document.addEventListener("keydown",(function(e){console.log("down",e)}))})();
 //# sourceMappingURL=script.js.map
